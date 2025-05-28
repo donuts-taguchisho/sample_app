c2e2784db534898f84f8cadb96e12dbd38bc2630
donuts-taguchisho <taguchi.sho@donuts.ne.jp>
2025-05-28 08:19:06 +0000

    new,createアクション追加、newビュー追加、CSS編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    return if @user.save
+
+    render 'new', status: :unprocessable_entity
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
